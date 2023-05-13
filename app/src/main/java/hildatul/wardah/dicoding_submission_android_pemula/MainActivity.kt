b8a30ab319454e462cdf34201742da53db095f92
Hilda003 <hildatulwardah003@gmail.com>
2023-05-14 01:16:45 +0700

    hshdkjas

---
+++ app/src/main/java/hildatul/wardah/dicoding_submission_android_pemula/MainActivity.kt
@@ -0,0 +1,11 @@
+package hildatul.wardah.dicoding_submission_android_pemula
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
